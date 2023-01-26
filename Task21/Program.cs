3f2302f2583e46e5145c8841bfe87af36b2e6b05
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 20:20:38 +0300

    add Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите x1: ");
+int x1 = int.Parse(ReadLine());
+Write("Введите y1: ");
+int y1 = int.Parse(ReadLine());
+Write("Введите z1: ");
+int z1 = int.Parse(ReadLine());
+Write("Введите x2: ");
+int x2 = int.Parse(ReadLine());
+Write("Введите y2: ");
+int y2 = int.Parse(ReadLine());
+Write("Введите z2: ");
+int z2 = int.Parse(ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0);
+
+WriteLine($"Длина отрезка = {d} ");
