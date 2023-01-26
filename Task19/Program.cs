fd42036bc45a1378f29892ea9a625a24b8d25b0c
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 21:47:16 +0300

    add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int numbers = int.Parse(ReadLine());
+int numbers_double = numbers;
+int number = 0;
+int sum = 0;
+
+while(numbers > 0)
+{
+    number = numbers % 10;
+    sum = (sum * 10) + number;
+    number = 0;  
+    numbers = numbers / 10; 
+     
+}
+
+if(numbers_double == sum)
+{
+    Write($"{numbers_double} -> да"); 
+}
+else
+{
+    Write($"{numbers_double} -> нет");
+}
