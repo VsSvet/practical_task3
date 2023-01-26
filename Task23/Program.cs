85772d3b84b490d4ce3c1bfffa0eac538b7a5f32
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 21:04:00 +0300

    add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+var list = new List<double>();
+int count = 1;
+
+while(count<number+1)
+{
+    list.Add(Math.Pow(count, 3));
+    count += 1;
+}
+
+WriteLine(String.Join(", ", list));
